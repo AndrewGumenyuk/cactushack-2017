58a57c76f3b44a7e64d918940172c8bf028dd917
Andrew Humeniuk <dj_revo_1@mail.ru>
2017-05-13 16:53:43 +0300

    Ad UserStatisticsController to HealthyFood.Application.Controllers

---
+++ HealthyFood/HealthyFood.Application/Controllers/UserStatisticsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Http;
+using HealthyFood.Services;
+using HealthyFood.ViewModels;
+
+namespace HealthyFood.Application.Controllers
+{
+    /// <summary>
+    /// Class that return the Statistic for User
+    /// </summary>
+    /// <returns></returns>
+    [RoutePrefix("api/user_statistics")]
+    public class UserStatisticsController : ApiController
+    {
+        /// <summary>
+        /// Method that return Statistic for User by route <see cref="api/user_statistics"/>
+        /// </summary>
+        /// <returns></returns>
+        [Route("")]
+        public Statistic Get()
+        {
+            MemoryCasheService memoryCasheService = new MemoryCasheService();
+            Profile profile = memoryCasheService.GetValue("Profile") as Profile;
+            if (profile == null)
+                throw new ArgumentException($"{nameof(profile)} can't null");
+            Statistic statistic = new Statistic(profile);
+
+            return statistic;
+        }
+    }
+}
