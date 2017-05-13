d29f8e0652c9a829fdfc8e856be9bbef3083812c
Andrew Humeniuk <dj_revo_1@mail.ru>
2017-05-13 15:40:56 +0300

    Add MemoryCasheService

---
+++ HealthyFood/HealthyFood.Domain/MemoryCasheService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace HealthyFood.Services
+{
+    public class MemoryCasheService
+    {
+        public object GetValue(string key)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            return memoryCache.Get(key);
+        }
+
+        public bool Add(string key, object value, DateTimeOffset absExpiration)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            return memoryCache.Add(key, value, absExpiration);
+        }
+        
+        public void Delete(string key)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            if (memoryCache.Contains(key))
+                memoryCache.Remove(key);
+        }
+    }
+}
