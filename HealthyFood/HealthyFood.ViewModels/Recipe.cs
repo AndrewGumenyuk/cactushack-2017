108222c802cd4f88c577820826ec280046e46567
Andrew Humeniuk <dj_revo_1@mail.ru>
2017-05-13 12:42:09 +0300

    #2 Add Recipe

---
+++ HealthyFood/HealthyFood.ViewModels/Recipe.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace HealthyFood.ViewModels
+{
+    public class Recipe
+    {
+        public Recipe(
+            string name,
+            string thumbnail,
+            string content)
+        {
+            Name = name;
+            Thumbnail = thumbnail;
+            Content = content;
+        }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; }
+
+        [JsonProperty(PropertyName = "thumbnail")]
+        public string Thumbnail { get; }
+
+        [JsonProperty(PropertyName = "content")]
+        public string Content { get; }
+    }
+}
