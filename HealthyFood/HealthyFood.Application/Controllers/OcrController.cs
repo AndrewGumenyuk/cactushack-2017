4440b70d3c8471e19bb6dbf50aefded7108fdcf6
ArturLavrov <arturstylus@gmail.com>
2017-05-13 12:33:08 +0300

    add:OcrService

---
+++ HealthyFood/HealthyFood.Application/Controllers/OcrController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.IO;
+using HealthyFood.Services;
+namespace HealthyFood.Application.Controllers
+{
+    public class OcrController : ApiController
+    {
+       public async Task<IHttpActionResult> Post()
+       {
+            var file = HttpContext.Current.Request.Files.Count > 0 ?
+                HttpContext.Current.Request.Files[0] : null;
+            if (file.ContentLength > 0)
+            {
+                OcrService ocrService = new OcrService();
+                BinaryReader binaryReader = new BinaryReader(file.InputStream);
+                byte[] imageAsByteArray = binaryReader.ReadBytes(file.ContentLength);
+                var json = await ocrService.GetOrcServerResponce(imageAsByteArray);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ HealthyFood/HealthyFood.Domain/OcrService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthyFood.Services
+{
+    public class OcrService
+    {
+        public async Task<string> GetOrcServerResponce(byte[] imageAsByteArray)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var content = new MultipartFormDataContent("Upload----" + DateTime.Now.ToString(CultureInfo.InvariantCulture)))
+                {
+                    content.Add(new StringContent("559afc6dc088957"), "\"apikey\"");
+                    content.Add(new StringContent("rus"), "\"language\"");
+                    content.Add(new StreamContent(new MemoryStream(imageAsByteArray)), "base64Image", "image.jpg");
+
+                    using (var message = await client.PostAsync("https://api.ocr.space/parse/image", content))
+                    {
+                        var json = await message.Content.ReadAsStringAsync();
+                        return json;
+                    }
+                }
+
+            }
+        }
+    }
+}
