78701e549584c601236954483786f867cfc0309c
Andrew Humeniuk <dj_revo_1@mail.ru>
2017-05-13 17:32:49 +0300

    Fix parsing eElements

--- HealthyFood/HealthyFood.Application/Controllers/OcrController.cs
+++ HealthyFood/HealthyFood.Application/Controllers/OcrController.cs
@@ -37,7 +37,7 @@ namespace HealthyFood.Application.Controllers
                 }
 
                 string parsedText = Regex.Replace(ocrDto.ParsedResults[0].ParsedText, @"\s+", "");
-                var eElements = Regex.Matches(ocrDto.ParsedResults.FirstOrDefault().ParsedText, "(([Е{IsCyrillic}]{1}[0-9]{3})|([E{IsCyrillic}-]{2}[0-9]{3}))").Cast<Match>().Select(m => m.Value).ToList();
+                var eElements = Regex.Matches(parsedText, "(([Е{IsCyrillic}]{1}[0-9]{3})|([E{IsCyrillic}-]{2}[0-9]{3}))").Cast<Match>().Select(m => m.Value).ToList();
 
                 return Ok(eElements);
             }
