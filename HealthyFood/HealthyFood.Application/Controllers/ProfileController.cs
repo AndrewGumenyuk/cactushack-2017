dabdf4b180de0943227193948a1a6a59fca2fbc9
Andrew Humeniuk <dj_revo_1@mail.ru>
2017-05-13 18:38:45 +0300

    Add/ Adjust Get/ UpdateProfile

--- HealthyFood/HealthyFood.Application/Controllers/ProfileController.cs
+++ HealthyFood/HealthyFood.Application/Controllers/ProfileController.cs
@@ -17,7 +17,7 @@ namespace HealthyFood.Application.Controllers
         /// </summary>`
         /// <returns>return bool was profile saved or no</returns>
         [Route("")]
-        public bool AddProfile([FromBody] Profile model)
+        public bool UpdateProfile([FromBody] Profile model)
         {
             if (model.Age <= 0)
                 throw new ArgumentException($"{nameof(model.Age)} can't be lass than 0");
@@ -27,9 +27,23 @@ namespace HealthyFood.Application.Controllers
                 throw new ArgumentException($"{nameof(model.Height)} can't be lass than 0");
 
             MemoryCasheService memoryCasheService = new MemoryCasheService();
-            memoryCasheService.Add("Profile", model, DateTimeOffset.MaxValue);
+            UpdateProfileByUser(model, memoryCasheService);
 
             return true;
+        }
+
+        private static void UpdateProfileByUser(Profile model, MemoryCasheService memoryCasheService)
+        {
+            memoryCasheService.Delete("Profile");
+            memoryCasheService.Add("Profile", model, DateTimeOffset.MaxValue);
+        }
+
+        [Route("")]
+        public Profile Get()
+        {
+            MemoryCasheService memoryCasheService = new MemoryCasheService();
+            Profile profile = memoryCasheService.GetValue("Profile") as Profile;
+            return profile;
         }
     }
 }
