2e6529c842a35c595de8c53bb61ece4057443f31
Andrew Humeniuk <dj_revo_1@mail.ru>
2017-05-13 20:02:27 +0300

    Adjust OcrService/ HealthyFood.ViewModels.csproj

--- HealthyFood/HealthyFood.Domain/OcrService.cs
+++ HealthyFood/HealthyFood.Domain/OcrService.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using HealthyFood.ViewModels;
+
 namespace HealthyFood.Services
 {
     public class OcrService
