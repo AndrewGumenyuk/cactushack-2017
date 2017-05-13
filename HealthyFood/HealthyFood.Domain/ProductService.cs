dad74182ec4375c7e301a0c15350ed21afc499ab
Andrew Humeniuk <dj_revo_1@mail.ru>
2017-05-13 19:46:26 +0300

    Add ProductService with mock data

---
+++ HealthyFood/HealthyFood.Domain/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using HealthyFood.ViewModels;
+using HealthyFood.ViewModels.Aggregates;
+
+namespace HealthyFood.Services
+{
+    public class ProductService
+    {
+        public ProductTypeAggregate[] GeTypeAggregatesByType(PhysicalActivity physicalActivity)
+        {
+            return new ProductTypeAggregate[]
+            {
+                new ProductTypeAggregate(PhysicalActivity.Low,
+                    new[]
+                    {
+                        new Product("Каша Геркулес", amount: 100, proteins: 65, carbonHydrates: 65.7, fat: 6.2,
+                            calories: 355),
+                        new Product("Зелень", amount: 100, proteins: 2.5, carbonHydrates: 5.2, fat: 0.4, calories: 36),
+                        new Product("Капуста брокколи варёная", amount: 100, proteins: 3, carbonHydrates: 4, fat: 0.4,
+                            calories: 27)
+                    }),
+                new ProductTypeAggregate(PhysicalActivity.Average,
+                    new[]
+                    {
+                        new Product("Горох Пассим колотый", amount: 100, proteins: 23, carbonHydrates: 48.1, fat: 1.6,
+                            calories: 299),
+                        new Product("Кабачок", amount: 100, proteins: 0.6, carbonHydrates: 4.6, fat: 0.3, calories: 24),
+                        new Product("Бананы сушёные", amount: 100, proteins: 3.9, carbonHydrates: 80.5, fat: 1.8,
+                            calories: 390)
+                    })
+            };
+        }
+    }
+}
