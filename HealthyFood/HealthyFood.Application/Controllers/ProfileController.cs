b1d0ce710fd4bfbcb7b0aee9e90a1c773f7eb1bc
Andrew Humeniuk <dj_revo_1@mail.ru>
2017-05-13 15:43:10 +0300

    Implement ProfileController

---
+++ HealthyFood/HealthyFood.Application/Controllers/ProfileController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Http;
+using HealthyFood.Services;
+using HealthyFood.ViewModels;
+
+namespace HealthyFood.Application.Controllers
+{
+    /// <summary>
+    /// Class that return the Profiles for User
+    /// </summary>
+    /// <returns></returns>
+    [RoutePrefix("api/profile")]
+    public class ProfileController : ApiController
+    {
+        /// <summary>
+        /// Method that return all Profile for User by route <see cref="api/profileuser/>
+        /// </summary>`
+        /// <returns>return bool was profile saved or no</returns>
+        [Route("")]
+        public bool AddProfile([FromBody] Profile model)
+        {
+            if (model.Age <= 0)
+                throw new ArgumentException($"{nameof(model.Age)} can't be lass than 0");
+            if (model.Weight <= 0)
+                throw new ArgumentException($"{nameof(model.Weight)} can't be lass than 0");
+            if (model.Height <= 0)
+                throw new ArgumentException($"{nameof(model.Height)} can't be lass than 0");
+
+            MemoryCasheService memoryCasheService = new MemoryCasheService();
+            memoryCasheService.Add("Profile", model, DateTimeOffset.MaxValue);
+
+            return true;
+        }
+    }
+}
